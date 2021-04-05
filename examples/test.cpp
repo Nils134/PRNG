b4646953f3d36fdf242a3f68bd1f3dfd77fb4e3e
Nils van den Honert <n.r.a.van.den.honert@umail.leidenuniv.nl>
2021-04-05 19:11:22 +0200

    Add cpp generator

---
+++ examples/test.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdlib.h>
+#include<fstream>
+
+void populateFile(int count,
+                  std::string const& file)
+{
+    std::ofstream myfile(file);
+    if (!myfile )
+    {
+       // Deal with error.
+       return;
+    }
+
+    for(int index=0; index<count; index++)
+    {
+        float random_integer = rand();
+        myfile << random_integer << "\n";
+    }
+}
+
+
+int main()
+{
+   // Seed the random number generator.
+   srand( time( NULL ) );
+
+   // Populate the file
+   std::string file("Random_cpp.txt");
+   populateFile(1000, file);
+   return 0;
+}
