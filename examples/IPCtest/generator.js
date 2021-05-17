b201e5916596eb514530506538b76ad3552d06e7
Nils van den Honert <n.r.a.van.den.honert@umail.leidenuniv.nl>
2021-05-17 18:37:38 +0200

    remove IPCtest folder

--- examples/IPCtest/generator.js
+++
@@ -1,9 +0,0 @@
-var readlineSync = require('readline-sync');
- 
-while(true) {
-// Wait for user's response.
-var userName = readlineSync.question('');
-for (var i = 0; i < 10; i++) {
-  console.log('Hi ' + userName + '!');
-  }
-}
