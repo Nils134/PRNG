b01b355a1629ea9da5cabe4beeb3cb7f8be3cde6
Nils van den Honert <n.r.a.van.den.honert@umail.leidenuniv.nl>
2021-05-25 18:06:32 +0200

    Add seed to random class

--- examples/hello/src/App.java
+++ examples/hello/src/App.java
@@ -1,19 +1,18 @@
-import java.util.Scanner;
-import java.lang.Math;
+import java.util.*;
 
 class Input {
     public static void main(String[] args) {
     	
         Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        random.setSeed(42);
     	while (true) {
             String inputstring =  input.next();
             for (int i = 0; i < 1000000; i++) {
-                String number = String.format("%.12f", Math.random());
+                String number = String.format("%.12f", random.nextDouble());
                 System.out.println(number);
             }
         }
         
-
-        // closing the scanner object
     }
 }
