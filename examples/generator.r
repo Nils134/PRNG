b104ef25ecba381fd20a194beb6fe09a1ba8f088
Nils van den Honert <s2413043@vuw.leidenuniv.nl>
2021-06-02 00:33:50 +0200

    Add r generator

--- examples/generator.r
+++ examples/generator.r
@@ -4,10 +4,12 @@ while(1) {
     open(f, blocking=TRUE)
     if (length(line <- readLines(f,n=1)) > 0) {
     for (i in 1:1000000) {
-        cat(format(round(runif(1), digits=12), nsmall = 12),"\n",
+        temp = paste(format(round(runif(1), digits=12), nsmall = 12),"\n",
             format(round(runif(1), digits=12), nsmall = 12),"\n",
             format(round(runif(1), digits=12), nsmall = 12),"\n",
-            format(round(runif(1), digits=12), nsmall = 12),"\n",)
+            format(round(runif(1), digits=12), nsmall = 12),"\n",
+            sep="")
+        cat(temp)
     }
     }
     
