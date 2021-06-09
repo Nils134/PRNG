3deb0776670c4b950f9a100b2a2d72d90b6a3bc3
Nils van den Honert <s2413043@vuw.leidenuniv.nl>
2021-06-09 17:20:31 +0200

    Fix R generator

--- examples/generator.r
+++ examples/generator.r
@@ -1,8 +1,10 @@
 set.seed(42)
 f <- file("stdin")
+open(f, blocking=FALSE)
+options(scipen = 999)
 while(1) {
-    open(f, blocking=TRUE)
-    if (length(line <- readLines(f,n=1)) > 0) {
+    
+    if (length(line <- readLines(f,ok=TRUE,n=1, skipNul=TRUE)) > 0) {
     for (i in 1:1000000) {
         temp = paste(format(round(runif(1), digits=12), nsmall = 12),"\n",
             format(round(runif(1), digits=12), nsmall = 12),"\n",
