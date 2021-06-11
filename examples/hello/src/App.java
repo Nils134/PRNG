1f2fbd8b10c9331a0d03547bfcea1eb8be2ac023
Nils van den Honert <s2413043@vuw.leidenuniv.nl>
2021-06-11 13:01:07 +0200

    Add java small crush document

--- examples/hello/src/App.java
+++ examples/hello/src/App.java
@@ -9,8 +9,8 @@ class Input {
     	while (true) {
             String inputstring =  input.next();
             for (int i = 0; i < 1000000; i++) {
-                String number = String.format("%.12f", random.nextDouble());
-                System.out.println(number);
+                String number = String.format("%.12f\n", random.nextDouble());
+                System.out.format(number);
             }
         }
         
